b9b0a75219db0224c284cd16ef8290384e491226
dbreness <duglasbrenes@gmail.com>
2016-10-25 11:44:51 -0600

    First
    ...

---
+++ www/js/controllers.js
@@ -0,0 +1,46 @@
+angular.module('starter.controllers', [])
+  .controller('AppCtrl', function($scope, $rootScope, AppService, $timeout ) {
+    $scope.showTabs = true;
+    $scope.loginData = {
+      username : 'admin',
+      password : '123'
+    };
+
+    $scope.scanCode = function () {
+      AppService.openScanner();
+    };
+    $scope.logout = function () {
+      AppService.logout();
+    };
+
+    $scope.nextStage = function ( stage ) {
+      var go2Screen = null;
+      switch ( stage ){
+        case 'admin':
+          go2Screen = 'usuario';
+          break;
+      }
+      $rootScope.goTo( 'tab.'+ go2Screen );
+    };
+
+    $rootScope.showTabs = AppService.getTabList();
+
+    $scope.login = function () {
+      var user = AppService.login( $scope.loginData.username, $scope.loginData.password );
+      if( user.isLoggedIn ){
+        var go2Screen = null;
+        switch ( user.type ){
+          case 'usuario':
+            $rootScope.showTabs.usuario = true;
+            $rootScope.showTabs.vehiculo = true;
+            $rootScope.showTabs.cita = true;
+            go2Screen = 'tab.usuario';
+            break;
+        }
+        $rootScope.goTo( go2Screen );
+      } else{
+        AppService.showMessage( 'Información errónea', 'Su usuario y/o contraseña son incorrectos, por favor intente de nuevo.' );
+      }
+    };
+  });
+
---
+++ www/js/services.js
@@ -0,0 +1,57 @@
+angular.module('starter.services', [])
+  .factory('AppService', function( $rootScope, $state, $ionicPopup ) {
+    AppService =  {
+      tabList : {},
+      init: function () {
+        AppService.setUser( { isLoggedIn : false } );
+        $rootScope.goTo = function ( whereTo ) {
+          $state.go( whereTo );
+          console.log( $rootScope.showTabs );
+        };
+        $rootScope.resetTabList = function () {
+          this.tabList = {
+            usuario : false,
+            vehiculo : false,
+            cita : false
+          };
+        };
+      },
+      getTabList: function () {
+        return AppService.tabList;
+      },
+      setTabList: function ( pTabList ) {
+        AppService.tabList = pTabList;
+      },
+      showMessage: function ( ptitle, ptemplate ) {
+        var alertPopup = $ionicPopup.alert({
+          title: ptitle,
+          template: ptemplate
+        });
+      },
+      logout: function () {
+        $rootScope.resetTabList();
+        AppService.setUser( { isLoggedIn : false } );
+        $rootScope.goTo( 'tab.login' );
+      },
+      login : function ( username, pass ) {
+        if( pass == "123" ){
+          $rootScope.resetTabList();
+          switch( username ){
+            case "admin":
+              $rootScope.user = { isLoggedIn : true, type : 'usuario' };
+              break;
+          }
+        }
+        return $rootScope.user;
+      },
+      getUser:  function (){
+        console.log( $rootScope.user );
+        return $rootScope.user;
+      },
+      setUser: function ( pUser ) {
+        $rootScope.user = pUser;
+      },
+    };
+    return AppService;
+  });
+
